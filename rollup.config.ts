c730d6833cb17229b9d0a6ac74437da0539efd6e
chuangyeyu <chuangyeyu@xiaoe-tech.com>
2022-10-12 15:38:05 +0800

    feat: 发布配置

--- rollup.config.ts
+++ rollup.config.ts
@@ -7,7 +7,7 @@ import json from 'rollup-plugin-json'
 
 const pkg = require('./package.json')
 
-const libraryName = 'ts-axios'
+const libraryName = 'axios'
 
 export default {
   input: `src/index.ts`,
